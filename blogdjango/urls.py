971e76491e892459c9094c01c004c1b3e889819b
arx_bullet <aranzav.c.b@gmail.com>
2022-02-15 19:20:38 -0300

    vistas, urls y templates principales

--- blogdjango/urls.py
+++ blogdjango/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inicio/', views.index, name='index'),
+    path('', views.index, name='inicio'),
+    path('about', views.about, name='sobre_nosotros')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,15 @@
+from urllib.request import Request
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    return render(request, 'mainapp/index.html', {
+        'title':'inicio'
+    })
+
+def about(request):
+    return render(request, 'mainapp/about.html', {
+        'title':'sobre, nosotros'
+    })
