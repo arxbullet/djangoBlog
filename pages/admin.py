50a8ec069b3e93890b870f9255d7ee8c3fd9eaa4
arx_bullet <aranzav.c.b@gmail.com>
2022-02-16 18:09:21 -0300

    configuraciones extra panel admin y nueva app blog

--- pages/admin.py
+++ pages/admin.py
@@ -3,6 +3,15 @@ from .models import *
 
 # Register your models here.
 
+#config nueva para mi modelo, personalizar busqueda modelos
+class pageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+    search_fields= ('title', 'content')
+    list_filter = ('visible',)
+    list_display = ('title', 'created_at') 
+    ordering = ('created_at')
+
+
 admin.site.register(Page) #registrar el modelo en panel admin django
 title = 'Administrador de paginas'
 subtitle = 'proyecto con django'
