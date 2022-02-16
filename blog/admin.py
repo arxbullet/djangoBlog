e3151ddf83c235d3bfe1c0c8cd7938c31f7c647f
arx_bullet <aranzav.c.b@gmail.com>
2022-02-16 18:34:15 -0300

    guardar usuario sin seleccionarlo

--- blog/admin.py
+++ blog/admin.py
@@ -9,9 +9,13 @@ class CategoryAdmin(admin.ModelAdmin):
     ordering = ('created_at',)
 
 class ArticleAdmin(admin.ModelAdmin):
-    readonly_fields = ('created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at', 'user')
     ordering = ('created_at',)
 
+    def save_model(self, request, obj, form, change):
+        if not obj.user_id : 
+            obj.user_id =  request.user_id
+        obj.save()
 
 admin.site.register(Article, ArticleAdmin) 
 admin.site.register(Category, CategoryAdmin) 
