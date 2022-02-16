edd091ac3f6b942c25689984184ac721858cf512
arx_bullet <aranzav.c.b@gmail.com>
2022-02-16 17:56:48 -0300

    ckeditor , enriquecer texto en consola admin djgo

--- pages/models.py
+++ pages/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
 class Page(models.Model):
     title = models.CharField(max_length=50, verbose_name='Titulo')
-    content = models.TextField( verbose_name='Contenido')
+    content = RichTextField( verbose_name='Contenido')
     slug = models.CharField(max_length=150, verbose_name='URL', unique=True)
     public = models.BooleanField(verbose_name='visible')
     created_at = models.DateTimeField(auto_now_add=True ,verbose_name='Creado el ')
