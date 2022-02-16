01f1649a9cae5620fd45ce96ed2d541aa4947bdf
arx_bullet <aranzav.c.b@gmail.com>
2022-02-16 14:51:52 -0300

    config panel de administracion

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Page) #registrar el modelo en panel admin django
+title = 'Administrador de paginas'
+subtitle = 'proyecto con django'
+
+admin.site.site_header = title
+admin.site.site_title = subtitle
+admin.site.index_title= subtitle
--- pages/apps.py
+++ pages/apps.py
@@ -1,6 +1,8 @@
+from tabnanny import verbose
 from django.apps import AppConfig
 
 
 class PagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pages'
+    verbose_name= 'Gestion de paginas' 
