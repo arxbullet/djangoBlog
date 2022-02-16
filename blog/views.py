417c50933a3070e6157b1120792023b74983fd98
arx_bullet <aranzav.c.b@gmail.com>
2022-02-16 18:45:41 -0300

    vistas blog

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from blog.models import Category, Article
 
 # Create your views here.
 
 def articles(request):
+
+    articles = Article.objects.all()
+
     return render(request, 'articles/list.html', {
-        'title' : 'articles'       
+        'title' : 'articles',
+        'articles' : articles
     })
