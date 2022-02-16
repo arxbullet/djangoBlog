ab9c555686c438c92cc2f790bb765a8dabe84af6
arx_bullet <aranzav.c.b@gmail.com>
2022-02-16 18:15:37 -0300

    crear modelos de blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
+from email.policy import default
 from django.db import models
+from ckeditor.fields import RichTextField
+
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Categoria')
+    description = models.CharField(max_length=50, verbose_name='Descripcion')
+    created_at = models.DateTimeField(auto_now_add=True ,verbose_name='Creada el ')
+    updated_at = models.DateTimeField(auto_now_add=True ,verbose_name='Actualizada el ')
+    
+    class Meta:
+        verbose_name='Categoria'
+        verbose_name_plural ='Categorias'
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Titulo')
+    content = RichTextField( verbose_name='Contenido')
+    image = models.ImageField(default='null', verbose_name='Imagen')
+    public = models.BooleanField(verbose_name='visible')
+    created_at = models.DateTimeField(auto_now_add=True ,verbose_name='Creado el ')
+    updated_at = models.DateTimeField(auto_now_add=True ,verbose_name='Actualizado el ')
+
+    class Meta:
+        verbose_name='Articulo'
+        verbose_name_plural ='Articulos'
+
+    def __str__(self):
+        return self.title
