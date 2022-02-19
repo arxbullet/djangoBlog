d53b5b45c1340796359be40fb3e51a5e9f86d777
arx_bullet <aranzav.c.b@gmail.com>
2022-02-19 20:39:20 -0300

    pagina para cada articulo

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     path('articulos', views.articles , name='list'),
     path('categories/<int:category_id>', views.category , name='categories'),
+     path('articulo/<int:article_id>', views.article , name='article'),
 ]
