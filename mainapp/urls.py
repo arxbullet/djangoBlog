09a851977ae367dea3d20f8eea12863318e93b92
arx_bullet <aranzav.c.b@gmail.com>
2022-02-21 15:59:02 -0300

    logout y login

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,4 +5,6 @@ urlpatterns=[
     path('inicio/', views.index, name='index'),
     path('', views.index, name='inicio'),
     path('registro', views.register_page, name='registro'),
+    path('login', views.login_page, name='login'),
+    path('logout', views.logout_user, name='logout'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,8 @@
+import re
 from django.shortcuts import render, redirect
 from mainapp.forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -24,9 +27,32 @@ def register_page(request):
 
             register_form.save()
 
+            messages.success(request, 'te has registrado correctamente')
+
             return redirect('index')
     
     return render(request, 'users/register.html', {
         'title': 'registro',
         'form' : register_form
     })
+
+def login_page(request):
+
+    if request.method == 'POST' :
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username , password=password)
+        if user is not None:
+            login(request, user)
+
+            return redirect('index')
+
+        else :  messages.error(request, 'usuario incorrecto')
+    
+    return render(request, 'users/login.html', {
+        'title': 'Login'
+    })
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
